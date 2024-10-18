5fa4a1999ce930289ea4b3866264cf72b10fb98e
ab-azmi <azmi.kiva@gmail.com>
2024-10-18 16:50:13 +0800

    install bootstrap

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+import './assets/themes/main.scss'
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
