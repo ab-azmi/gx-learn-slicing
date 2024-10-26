c6d318fd8b7462a329f1af58437143ab1a13fc5b
ab-azmi <azmi.kiva@gmail.com>
2024-10-26 11:04:26 +0800

    sync dark mode between multiple tabs

--- src/context/DarkModeProvider.tsx
+++ src/context/DarkModeProvider.tsx
@@ -1,42 +1,60 @@
-import {createContext, ReactNode, useEffect, useState} from "react";
+import { createContext, ReactNode, useEffect, useState } from "react";
 
 type Props = {
-    children: ReactNode;
-}
+  children: ReactNode;
+};
 
 type IDarkModeContext = {
-    darkMode: string;
-    setDarkMode: (darkMode: string) => void;
-}
+  darkMode: string;
+  setDarkMode: (darkMode: string) => void;
+};
 
 const getDarkModeCached = () => {
-    const darkMode = localStorage.getItem("darkMode");
-    if (darkMode) return darkMode;
-    return "auto";
-}
+  const darkMode = localStorage.getItem("darkMode");
+  if (darkMode) return darkMode;
+  return "light";
+};
 
 const initialValue: IDarkModeContext = {
-    darkMode: getDarkModeCached(),
-    setDarkMode: () => {}
-}
+  darkMode: getDarkModeCached(),
+  setDarkMode: () => {},
+};
 
 // Context
 const DarkModeContext = createContext<IDarkModeContext>(initialValue);
 
 // Provider
-const DarkModeProvider = ({children}: Props) => {
-    const [darkMode, setDarkMode] = useState(getDarkModeCached());
+const DarkModeProvider = ({ children }: Props) => {
+  const [darkMode, setDarkMode] = useState(getDarkModeCached());
 
-    useEffect(() => {
-        document.documentElement.setAttribute("data-bs-theme", darkMode);
-        localStorage.setItem("darkMode", darkMode);
-    }, [darkMode]);
+  useEffect(() => {
+    const onStorageUpdate = () => {
+      // Update the local storage
+      const mode = localStorage.getItem("darkMode");
+      if (mode) {
+        setDarkMode(mode);
+        document.documentElement.setAttribute("data-bs-theme", mode);
+      }
+    };
 
-    return (
-        <DarkModeContext.Provider value={{darkMode, setDarkMode}}>
-            {children}
-        </DarkModeContext.Provider>
-    )
-}
+    // Listen for changes in local storage
+    window.addEventListener("storage", onStorageUpdate);
 
-export {DarkModeProvider, DarkModeContext};
+    // Set the theme on the document
+    document.documentElement.setAttribute("data-bs-theme", darkMode);
+    localStorage.setItem("darkMode", darkMode);
+
+    return () => {
+      // Remove the listener when the component is unmounted
+      window.removeEventListener("storage", onStorageUpdate);
+    };
+  }, [darkMode]);
+
+  return (
+    <DarkModeContext.Provider value={{ darkMode, setDarkMode }}>
+      {children}
+    </DarkModeContext.Provider>
+  );
+};
+
+export { DarkModeProvider, DarkModeContext };
