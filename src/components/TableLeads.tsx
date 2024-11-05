da98fe81acda43465e42179ef00a37dac2d3098d
ab-azmi <azmi.kiva@gmail.com>
2024-11-05 10:00:51 +0800

    integrate tabs to leads table

--- src/components/TableLeads.tsx
+++ src/components/TableLeads.tsx
@@ -83,19 +83,16 @@ const TableLeads = ({
     <div className="p-3 p-3 bg-secondary rounded-4">
       <div className="d-flex gap-3 align-items-end justify-content-between mb-2">
         <h5 className="fw-bold">Manage Leads</h5>
-        <div>
+        <div className="d-flex align-items-center gap-2">
+          <Button style="outline">Generate Summary</Button>
           {onAdd && (
-            <button
-              type="button"
-              className="btn btn-primary"
-              onClick={() => onAdd()}
-            >
+            <Button type="button" style="fill" onClick={() => onAdd()}>
               Add
-            </button>
+            </Button>
           )}
         </div>
       </div>
-      <div className="d-flex gap-3 align-items-end mb-2">
+      <div className="d-flex gap-3 align-items-end mb-3">
         <div className="w-30">
           <Input
             type="text"
@@ -130,156 +127,212 @@ const TableLeads = ({
             <button className="btn px-0">
               <Filter size="24" />
             </button>
-            <button className="btn px-0" onClick={() => onClearFilter()}>
+            <button
+              className="btn px-0 text-decoration-underline"
+              onClick={() => onClearFilter()}
+            >
               Clear All
             </button>
           </>
         )}
       </div>
-      <div className="table-responsive mt-3">
-        <table className="table">
-          <thead>
-            <tr className="">
-              <th className="bg-muted w-20 fw-medium"># Leads</th>
-              <th className="bg-muted w-20 fw-medium">Address</th>
-              <th className="bg-muted w-20 fw-medium">Status</th>
-              <th className="bg-muted w-20 fw-medium">Info</th>
-              <th className="bg-muted w-20 fw-medium">Created</th>
-              <th className="bg-muted"></th>
-            </tr>
-          </thead>
-          <tbody>
-            {data.length > 0 ? (
-              data?.slice(skip, upper).map((item, index) => (
-                <tr key={index}>
-                  <td>
-                    <span className="text-primary">
-                      #{getNestedValue(item, "code")}
-                    </span>
-                    <div className="d-flex flex-column gap-1 mt-3">
-                      <span className="text-muted text-xs">Branch Office</span>
-                      <span className="text-capitalize">
-                        Globalxtreme {getNestedValue(item, "branch")}
-                      </span>
-                    </div>
-                  </td>
-                  <td className="px-3">
-                    <div className="d-flex flex-column gap-1">
-                      <span className="text-capitalize fw-medium">
-                        {getNestedValue(item, "branch")}
-                      </span>
-                      <span className="text-muted">
-                        {getNestedValue(item, "address")}
-                      </span>
-                    </div>
-                    <div className="d-flex flex-column gap-1 mt-3">
-                      <span className="text-capitalize fw-medium">Phone</span>
-                      <span className="text-muted">
-                        {getNestedValue(item, "phone")}
-                      </span>
-                    </div>
-                  </td>
-                  <td>
-                    <span className="badge fs-6 bg-info text-capitalize fw-medium">
-                      {getNestedValue(item, "status.name")}
-                    </span>
-                    <div className="d-flex flex-column gap-1 mt-3">
-                      <span className="text-muted">Probability</span>
-                      <span className="text-uppercase fw-semibold text-decoration-underline text-info">
-                        #{getNestedValue(item, "probability.name")}
-                      </span>
-                    </div>
-                  </td>
-                  <td>
-                    <div
-                      className="d-flex flex-column gap-1"
-                      style={{ width: "fit-content" }}
-                    >
-                      <span className="text-muted">Type</span>
-                      <div className="d-flex gap-1">
-                        <span className="badge fs-6 bg-info text-capitalize fw-medium">
-                          {getNestedValue(item, "type.name")}
-                        </span>
-                        <button className="border-0 bg-transparent">
-                          <Edit size="20" />
-                        </button>
-                      </div>
-                    </div>
-                    <div
-                      className="d-flex flex-column gap-1 mt-3"
-                      style={{ width: "fit-content" }}
-                    >
-                      <span className="text-muted">Note</span>
-                      <span className="text-capitalize">
-                        {getNestedValue(item, "note")}
-                      </span>
-                    </div>
-                  </td>
-                  <td>
-                    <div className="d-flex gap-2 align-items-center">
-                      <img
-                        src={User}
-                        alt=""
-                        className="rounded-circle object-fit-cover"
-                        style={{ width: "40px", height: "40px" }}
-                      />
-                      <div className="text-xs text-capitalize d-flex flex-column">
-                        <span className="fw-bold">
-                          {getNestedValue(item, "name")}
-                        </span>
-                        <span className="fw-light">
-                          {formatDate(
-                            getNestedValue(item, "created_at"),
-                            "dd MMM yyyy HH:mm"
-                          )}
-                        </span>
-                      </div>
-                    </div>
-                  </td>
-                  <td>
-                    <Button type="button" style="fill">
-                      Report
-                    </Button>
-                    <div className="d-flex gap-1 mt-3">
-                      {onEdit && (
-                        <button
-                          type="button"
-                          className="btn btn-sm text-muted"
-                          onClick={() => onEdit(item)}
-                        >
-                          <Edit size="24" variant="Bulk" />
-                        </button>
-                      )}
-                      {onDelete && (
-                        <button
-                          type="button"
-                          className="btn btn-sm text-danger"
-                          onClick={() => onDelete(item)}
-                        >
-                          <Trash size="24" variant="Bulk" />
-                        </button>
-                      )}
-                    </div>
-                  </td>
+      <ul className="nav custom-tab mb-3" id="pills-tab" role="tablist">
+        <li className="nav-item tab-item" role="presentation">
+          <button
+            className="tab-link active"
+            id="pills-home-tab"
+            data-bs-toggle="pill"
+            data-bs-target="#pills-home"
+            type="button"
+            role="tab"
+            aria-controls="pills-home"
+            aria-selected="true"
+          >
+            All Leads
+          </button>
+        </li>
+        <li className="nav-item tab-item" role="presentation">
+          <button
+            className="tab-link"
+            id="pills-profile-tab"
+            data-bs-toggle="pill"
+            data-bs-target="#pills-profile"
+            type="button"
+            role="tab"
+            aria-controls="pills-profile"
+            aria-selected="false"
+          >
+            From MyGX App
+          </button>
+        </li>
+      </ul>
+      <div className="tab-content" id="pills-tabContent">
+        <div
+          className="tab-pane fade show active py-2"
+          id="pills-home"
+          role="tabpanel"
+          aria-labelledby="pills-home-tab"
+        >
+          <div className="table-responsive">
+            <table className="table">
+              <thead>
+                <tr className="">
+                  <th className="bg-muted w-20 fw-medium"># Leads</th>
+                  <th className="bg-muted w-20 fw-medium">Address</th>
+                  <th className="bg-muted w-20 fw-medium">Status</th>
+                  <th className="bg-muted w-20 fw-medium">Info</th>
+                  <th className="bg-muted w-20 fw-medium">Created</th>
+                  <th className="bg-muted"></th>
                 </tr>
-              ))
-            ) : (
-              <tr>
-                <td colSpan={columns.length + 1} className="text-center">
-                  No data available
-                </td>
-              </tr>
-            )}
-          </tbody>
-        </table>
-      </div>
-      <div className="mt-2">
-        <TablePagination
-          total={data.length}
-          limit={limit}
-          page={page}
-          setPage={handleChangePage}
-        />
+              </thead>
+              <tbody>
+                {data.length > 0 ? (
+                  data?.slice(skip, upper).map((item, index) => (
+                    <tr key={index}>
+                      <td>
+                        <span className="text-primary">
+                          #{getNestedValue(item, "code")}
+                        </span>
+                        <div className="d-flex flex-column gap-1 mt-3">
+                          <span className="text-muted text-xs">
+                            Branch Office
+                          </span>
+                          <span className="text-capitalize">
+                            Globalxtreme {getNestedValue(item, "branch")}
+                          </span>
+                        </div>
+                      </td>
+                      <td className="px-3">
+                        <div className="d-flex flex-column gap-1">
+                          <span className="text-capitalize fw-medium">
+                            {getNestedValue(item, "branch")}
+                          </span>
+                          <span className="text-muted">
+                            {getNestedValue(item, "address")}
+                          </span>
+                        </div>
+                        <div className="d-flex flex-column gap-1 mt-3">
+                          <span className="text-capitalize text-muted">
+                            Phone
+                          </span>
+                          <span className="fw-medium">
+                            {getNestedValue(item, "phone")}
+                          </span>
+                        </div>
+                      </td>
+                      <td>
+                        <span className="badge fs-6 bg-info text-capitalize fw-medium">
+                          {getNestedValue(item, "status.name")}
+                        </span>
+                        <div className="d-flex flex-column gap-1 mt-3">
+                          <span className="text-muted">Probability</span>
+                          <span className="text-uppercase fw-semibold text-decoration-underline text-info">
+                            #{getNestedValue(item, "probability.name")}
+                          </span>
+                        </div>
+                      </td>
+                      <td>
+                        <div
+                          className="d-flex flex-column gap-1"
+                          style={{ width: "fit-content" }}
+                        >
+                          <span className="text-muted">Type</span>
+                          <div className="d-flex gap-1">
+                            <span className="badge fs-6 bg-info text-capitalize fw-medium">
+                              {getNestedValue(item, "type.name")}
+                            </span>
+                            <button className="border-0 bg-transparent">
+                              <Edit size="20" />
+                            </button>
+                          </div>
+                        </div>
+                        <div
+                          className="d-flex flex-column gap-1 mt-3"
+                          style={{ width: "fit-content" }}
+                        >
+                          <span className="text-muted">Note</span>
+                          <span className="text-capitalize">
+                            {getNestedValue(item, "note")}
+                          </span>
+                        </div>
+                      </td>
+                      <td>
+                        <div className="d-flex gap-2 align-items-center">
+                          <img
+                            src={User}
+                            alt=""
+                            className="rounded-circle object-fit-cover"
+                            style={{ width: "40px", height: "40px" }}
+                          />
+                          <div className="text-xs text-capitalize d-flex flex-column">
+                            <span className="fw-bold">
+                              {getNestedValue(item, "name")}
+                            </span>
+                            <span className="fw-light">
+                              {formatDate(
+                                getNestedValue(item, "created_at"),
+                                "dd MMM yyyy HH:mm"
+                              )}
+                            </span>
+                          </div>
+                        </div>
+                      </td>
+                      <td>
+                        <Button type="button" style="fill">
+                          Report
+                        </Button>
+                        <div className="d-flex gap-1 mt-3">
+                          {onEdit && (
+                            <button
+                              type="button"
+                              className="btn btn-sm text-muted"
+                              onClick={() => onEdit(item)}
+                            >
+                              <Edit size="24" variant="Bulk" />
+                            </button>
+                          )}
+                          {onDelete && (
+                            <button
+                              type="button"
+                              className="btn btn-sm text-danger"
+                              onClick={() => onDelete(item)}
+                            >
+                              <Trash size="24" variant="Bulk" />
+                            </button>
+                          )}
+                        </div>
+                      </td>
+                    </tr>
+                  ))
+                ) : (
+                  <tr>
+                    <td colSpan={columns.length + 1} className="text-center">
+                      No data available
+                    </td>
+                  </tr>
+                )}
+              </tbody>
+            </table>
+          </div>
+          <div className="mt-2">
+            <TablePagination
+              total={data.length}
+              limit={limit}
+              page={page}
+              setPage={handleChangePage}
+            />
+          </div>
+        </div>
+        <div
+          className="tab-pane fade"
+          id="pills-profile"
+          role="tabpanel"
+          aria-labelledby="pills-profile-tab"
+        >
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet
+          suscipit odio quam officia tempora ducimus deserunt debitis laborum
+          dignissimos temporibus?
+        </div>
       </div>
     </div>
   );
